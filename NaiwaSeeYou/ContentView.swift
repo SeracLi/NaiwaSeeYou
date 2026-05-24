135898cf8ef9afc738e0a5be5fb08c77800cebb4
Xiang <1159531307@qq.com>
2026-05-24 13:59:11 +0800

    Initial Commit

---
+++ NaiwaSeeYou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NaiwaSeeYou
+//
+//  Created by Serac on 2026/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NaiwaSeeYou/NaiwaSeeYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaiwaSeeYouApp.swift
+//  NaiwaSeeYou
+//
+//  Created by Serac on 2026/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NaiwaSeeYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
